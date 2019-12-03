5f3914132e0e01b102452e774752590c3e02c634
Shubhangi Yadav <shubhangi.yadav@sjsu.edu>
2019-12-03 00:36:58 -0800

    Added fetch data apis and added s3 upload code

--- backend/bin/OCRecog/improveContrast.py
+++
@@ -1,8 +0,0 @@
-import Image, ImageEnhance
-
-image = Image.open('denoised.jpg')
-enhancer = ImageEnhance.Sharpness(image)
-
-image = enhancer.enhance(2.0)
-
-image.save('modified.jpg')
--- backend/bin/OCRecog/spellCheck2.py
+++
@@ -1,7 +0,0 @@
-from spellchecker import SpellChecker
-
-spell = SpellChecker()
-# find those words that may be misspelled
-misspelled = spell.unknown(['statment',  'is', 'wrong'])
-for word in misspelled:
-    print(spell.correction(word))
